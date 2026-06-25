9841e211b8a7d3ec8e9c2e7030963a28f794bbbb
Pulagam Pravallika <pravallikapulagam16@gmail.com>
2026-06-25 15:33:14 +0530

    Added logging middleware

--- notification-app-fe/src/utils/logger.js
+++ notification-app-fe/src/utils/logger.js
@@ -1,17 +1,20 @@
-export async function fetchNotifications() {
-  const response = await fetch(
-    "http://4.224.186.213/evaluation-service/notifications",
-    {
+export async function Log(stack, level, packageName, message) {
+  try {
+    await fetch("http://4.224.186.213/evaluation-service/logs", {
+      method: "POST",
       headers: {
+        "Content-Type": "application/json",
         Authorization:
           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJwcmF2YWxsaWthcHVsYWdhbTE2QGdtYWlsLmNvbSIsImV4cCI6MTc4MjM4MjUyOCwiaWF0IjoxNzgyMzgxNjI4LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiOTcyZDFiOTMtYWVkNS00N2QwLTllM2ItMDAxZWJmMWVkODQwIiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoicHVsYWdhbSBwcmF2YWxsaWthIiwic3ViIjoiZjE2NjJjM2ItOTgyNy00M2RjLWI5M2UtOTI2ZTA5ODFhOTA0In0sImVtYWlsIjoicHJhdmFsbGlrYXB1bGFnYW0xNkBnbWFpbC5jb20iLCJuYW1lIjoicHVsYWdhbSBwcmF2YWxsaWthIiwicm9sbE5vIjoiMjNiMDFhMDVlOCIsImFjY2Vzc0NvZGUiOiJhaFhqdnAiLCJjbGllbnRJRCI6ImYxNjYyYzNiLTk4MjctNDNkYy1iOTNlLTkyNmUwOTgxYTkwNCIsImNsaWVudFNlY3JldCI6IlVSVWtUTlNQZXRNYXlhcHMifQ.UwYmTPRkAaW3_HIS7G_RexgspDkkm0huaYvQycK5eHM",
       },
-    }
-  );
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch notifications");
+      body: JSON.stringify({
+        stack,
+        level,
+        package: packageName,
+        message,
+      }),
+    });
+  } catch (err) {
+    console.error("Logging failed:", err);
   }
-
-  return await response.json();
 }
